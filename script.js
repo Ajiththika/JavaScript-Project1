e0d05bb8b35c936fc6c5e80345c19c1605e69474
Ajiththika <ajiththika17@gmail.com>
2025-11-06 12:37:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("text").innerHTML="This is written usingdocument.write()"
+// window.alert("Hello welcome my website!")
+document.write
+
+console.log("JavaScript is fun!");
+
+var name="Ajie"
+let age="20"
+const sub="It"
+var name="Ajiththi"
+let age1="21"
+const subject="English"
+console.log(name);
+console.log(age);
+console.log(sub);
+var favcolor= prompt("Type your favourite color")
+document.body.style.backgroundColor = favcolor
+console.log(favcolor)
+
+var price="100"
+var quantity="5"
+var Total=price * quantity
+console.log(Total)
